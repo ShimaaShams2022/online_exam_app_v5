a287da828e89d1c58f67a1493240fe120c919e7e
shimaa <shimaa.shams@gmail.com>
2024-10-27 16:10:54 +0300

    feat: get all subjects by user token

--- lib/domain/repository/register_usecase.dart
+++ lib/domain/repository/register_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:injectable/injectable.dart';
+import 'package:online_exam_app_v5/domain/models/auth_response_domain_model.dart';
 
 import '../common/api_results.dart';
 import '../models/user.dart';
@@ -9,7 +10,7 @@ class RegisterUseCase{
   AuthRepository authRepository;
   RegisterUseCase(this.authRepository);
 
-  Future<Result<User?>> invoke(String  username,String  firstname,
+  Future<Result<AuthResponse?>> invoke(String  username,String  firstname,
   String  lastname,String  email,String  phone,String password,String  rePassword){
     return authRepository.register(username, firstname, lastname, email, phone, password, rePassword);
   }
